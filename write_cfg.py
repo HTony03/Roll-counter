0197f712f3791076bb2e16acdf82f969241e127a
HTony03 <80151962+HTony03@users.noreply.github.com>
2023-03-17 21:11:31 +0800

    Add files via upload

---
+++ write_cfg.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 16 10:07:52 2023
+
+@author: Administrator
+"""
+
+import configparser,os
+cfg_file = r"C:\\random.cfg"
+conf = configparser.ConfigParser()
+cfgfile = open(cfg_file,"w")
+conf.add_section("csv_route")
+conf.set("csv_route","CSV_all",r"C:\rand\all.csv")
+conf.set("csv_route","CSV_girls",r"C:\rand\girl.csv")
+conf.set("csv_route","CSV_boys",r"C:\rand\boy.csv")
+conf.write(cfgfile)
+cfgfile.close()
+print("config saved to ",(cfg_file))
+os.system("pause")
